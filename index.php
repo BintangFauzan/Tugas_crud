bef9ec6a35f380e8bc58f08713437ebbb5260980
Bintang fauzan <76691158+BintangFauzan@users.noreply.github.com>
2022-06-04 16:23:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-    // $a =$_GET['a'];
-    // $b =$_GET['b'];
-    // $c= $a * $b;
-
-    // $reply = [
-    // 	'a' => $a,
-    // 	'b' => $b,
-    // 	'hasil' => $c
-    // ];
-
-    // header('Content-Type: application/json');
-    // echo json_encode($reply);
-
-// phpinfo();
-
     $dbHost="localhost";  
     $dbName="kuliah_web";  
     $dbUser="root";      //by default root is user name.  
@@ -28,20 +13,4 @@
     } catch(Exception $e){  
     Echo "Koneksi Gagal" . $e->getMessage();  
     }  
-
-
-// $dbserver = "localhost";
-// $dbname = "kuliah_web";
-// $dbuser = "root";
-// $dbpassword = "";
-// $dsn = "mysql:host={$dbserver};dbname={$dbname}";
-
-// $connection = null;
-// try {
-//   $connection = new PDO($dsn, $dbuser, $dbpassword);
-
-// } catch(Exception $exception){
-//   die("Terjadi error: {$exception->getMessage()}");
-// }
-
 ?> 
