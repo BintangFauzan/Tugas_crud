ba9d29247f490b776ddd00bf1de125da42b808a6
Bintang fauzan <76691158+BintangFauzan@users.noreply.github.com>
2022-06-05 22:02:01 +0700

    Update buku.php

--- buku.php
+++ buku.php
@@ -9,19 +9,6 @@ $result = $statement->fetchALL();
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($result);
 
-// include_once 'index.php';
-
-// $query = "select * from buku";
-
-// $statement = $dbConn->query($query);
-
-// $statement->setfetchMode(PDO::FETCH_OBJ);
-
-// $result = $statement->fetchALL();
-
-// header('Content-Type: application/json');
-
-// echo json_encode($result);
 
 
 ?>
